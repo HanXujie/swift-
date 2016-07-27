4e14adb087af3ede975594a3a971687242081449
HanXujie <850318868@qq.com>
2016-07-27 23:06:13 +0800

    no message

--- swift-closure/swift-closure/ListTableViewController.swift
+++ swift-closure/swift-closure/ListTableViewController.swift
@@ -10,15 +10,87 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
 
+    var list:[Player]?
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadData { (players) -> () in
+            self.list = players
+            // 刷新数据
+            self.tableView.reloadData()
+        }
     }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let dtVC = segue.destinationViewController as! DetailViewController
+        
+        let indexPath = tableView.indexPathForSelectedRow
+        
+        guard let idx = indexPath else{
+            
+            return
+        }
+        
+        let model = list![idx.row]
+        
+        dtVC.model = model
+        
+        let homeClosure = {
+            
+            self.tableView.reloadData()
+        }
+        
+        dtVC.closure = homeClosure
+        
+    }
+    
+    
+    // 网络请求加载数据--模拟
+    func loadData(finished: (players: [Player])->()){
+        
+        // GCD异步加载数据
+        dispatch_async(dispatch_get_global_queue(0, 0)) { () -> Void in
+            
+            // 睡眠两秒钟
+            NSThread.sleepForTimeInterval(2)
+            
+            // 初始化一个数组
+            var array: [Player] = [Player]()
+            
+            // 拼接数据
+            for i in 0...50{
+                // 名字
+                let name = "NO.\(i)"
+                // 分数
+                let score = random()%100
+                
+                let dict: [String : AnyObject] = ["name":name,"score":score]
+                
+                // 字典转模型
+                let p = Player(dict: dict)
+                
+                array.append(p)
+                
+            }
+            
+            // 回到主线程回调
+            dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                
+                finished(players: array)
+            })
+            
+            
+            
+            
+        }
+        
+        
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -29,23 +101,28 @@ class ListTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return list?.count ?? 0
     }
 
-    /*
+  
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as!PlayerTableViewCell
+        
+        cell.model = list![indexPath.row]
 
         return cell
     }
-    */
+
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return 100.0
+    }
 
     /*
     // Override to support conditional editing of the table view.
